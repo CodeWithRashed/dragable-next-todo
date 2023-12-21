82771a200bf641d0b4336138577be8690d972b30
Md Rashed Ali <tools.rashed@gmail.com>
2023-12-21 11:32:29 +0600

    css variable added

---
+++ src/components/Banner.tsx
@@ -0,0 +1,11 @@
+
+
+const Banner = () => {
+  return (
+    <div className="bg-[#E5F4F2]">
+      
+    </div>
+  )
+}
+
+export default Banner
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,28 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        "primary-bg:": "var(--primary-bg:)",
+        "btn-primary-bg": "var(--btn-primary-bg)",
+        "btn-primary-light": "var(--btn-primary-light)",
+        "secondary-color": "var(--secondary-color:)",
+        "text-primary": "var(--text-primary:)",
+        "text-secondary": "var(--text-secondary:)",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
