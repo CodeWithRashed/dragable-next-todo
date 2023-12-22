a608f4537d53f22e1f129e5d1576d65e244b030f
Md Rashed Ali <tools.rashed@gmail.com>
2023-12-22 16:05:41 +0600

    route protechted

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,9 @@
 import Banner from '@/components/Banner'
 import Navbar from '@/components/Navbar'
-import Image from 'next/image'
-import { getServerSession } from 'next-auth'
-import { options } from './api/auth/[...nextauth]/options'
 import Footer from '@/components/Footer'
 
 export default async function  Home() {
-  const session =  await getServerSession(options)
-  console.log(session?.user)
+
   return ( 
     <main className="">
       <section className='header bg-[#F8D57E]'>
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,28 +1,27 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-
-
 export function middleware(request: NextRequest) {
   const pathName = request.nextUrl.pathname;
 
   const isPublicPath = pathName === "/login" || pathName === "/signup";
-  const token = request.cookies.get("token")?.value || "";
+  const token = request.cookies.get("next-auth.session-token")?.value || "";
 
-  
-  //Handle Public Request
-  // if (!isPublicPath && token) {
-  //   return NextResponse.redirect(new URL("/dashboard", request.nextUrl));
-  // }
+  // Redirect to the dashboard if it's not a public path and there's a valid token
+  if (!isPublicPath && token) {
+    return null;
+  }
 
-  // //handle private path request
-  // if (!isPublicPath && !token) {
-  //   if (pathName === "/") {
-  //     return null;
-  //   } else {
-  //     return NextResponse.redirect(new URL("/login", request.nextUrl));
-  //   }
-  // }
+  // Redirect to the login page if it's not a public path and there's no valid token
+  if (!isPublicPath && !token) {
+    if (pathName === "/") {
+      return null; // Allow access to the home page without authentication
+    } else {
+      return NextResponse.redirect(new URL("/login", request.nextUrl));
+    }
+  }
+
+  // For public paths or when there's a valid token, do nothing and allow the request
 }
 
 // See "Matching Paths" below to learn more
